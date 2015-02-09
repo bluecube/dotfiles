06409e56d559eae3aa943bb72517503e9c57b5f7
Kuba Marek <blue.cube@seznam.cz>
2015-02-09 09:47:35 +0100

    Fixed the instalation script for use with vundle as submodule.

--- install.sh
+++ install.sh
@@ -3,18 +3,27 @@
 cd $(dirname $0)
 source_dir=$(pwd)
 
-find -type f | while read f
-do
-    if [[ $f = ./install.sh ]] ; then continue ; fi
-    if [[ $f = ./.git/* ]] ; then continue ; fi
-    if [[ $f = ./.gitmodules ]] ; then continue ; fi
+function process()
+{
+    f=$1
 
     source_file=$source_dir/$f
     target_file=~/$f
 
     echo "Installing $source_file to $target_file"
 
-    rm -f $target_file
+    rm -rf $target_file
     mkdir -p $(dirname $target_file)
     ln -s $source_file $target_file
+}
+
+find -type f | while read f
+do
+    if [[ $f = ./install.sh ]] ; then continue ; fi
+    if [[ $f = ./.git/* ]] ; then continue ; fi
+    if [[ $f = ./.gitmodules ]] ; then continue ; fi
+    if [[ $f = ./.vim/bundle/* ]] ; then continue ; fi
+    process $f
 done
+
+process .vim/bundle/Vundle.vim
